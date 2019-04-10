ad59cedd2e2359fddc315939f1ec950b15380da5
sunshine <qiaoliuyang@souguakeji.com>
2019-04-10 19:49:04 +0800

    添加json解析工具

--- app/src/main/java/cn/example/sunshine/MainActivity.kt
+++ app/src/main/java/cn/example/sunshine/MainActivity.kt
@@ -1,16 +1,11 @@
 package cn.example.sunshine
 
-import android.graphics.Color
 import android.view.View
 import cn.example.sunshine.base.BaseActivity
 import cn.example.sunshine.base.BaseContract
-import cn.example.sunshine.bean.User
 import cn.example.sunshine.extension.startActivity
 import cn.example.sunshine.ui.TestActivity
-import cn.example.sunshine.utils.SharedPreferences
-import cn.example.sunshine.utils.setStatusBarColor
-import cn.example.sunshine.utils.toJsonArray
-import cn.example.sunshine.utils.toJsonObject
+import cn.example.sunshine.utils.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -19,18 +14,15 @@ import kotlinx.android.synthetic.main.activity_main.*
  * @desc
  */
 
+
 class MainActivity : BaseActivity<BaseContract.BasePresenter>(), View.OnClickListener {
+
     var isLogin: Boolean by SharedPreferences.preference(this, "islogin", false)
-    val json: String = "{\"name\":\"name0\",\"age\":\"0\"}"
-    val jsonArray: String = "[{\"name\":\"name1\",\"age\":\"intvalue\"},{\"name\":\"name2\",\"age\":\"intvalue\"},{\"name\":\"name3\",\"age\":\"intvalue\"}]"
+
     override fun getLayout() = R.layout.activity_main
+
     override fun initView() {
         super.initView()
-        setStatusBarColor(this, Color.CYAN)
-        val user: User = toJsonObject(json, User::class.java)
-        val users: List<User> = toJsonArray(jsonArray, User::class.java)
-        print(user.toString())
-        print(users.toString())
     }
 
     override fun onResume() {
--- app/src/main/java/cn/example/sunshine/utils/GsonUtils.kt
+++ app/src/main/java/cn/example/sunshine/utils/GsonUtils.kt
@@ -3,11 +3,13 @@ package cn.example.sunshine.utils
 import android.text.TextUtils
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.gson.JsonParser
+
 
 /**
  * @author SunShine-Joex
  * @date   2019/3/15
- * @desc
+ * @desc   Gson 封装的json解析工具
  */
 fun <T> toJsonObject(jsonString: String, t: Class<T>): T {
     if (TextUtils.isEmpty(jsonString)) {
@@ -17,6 +19,25 @@ fun <T> toJsonObject(jsonString: String, t: Class<T>): T {
     }
 }
 
+/**
+ *  此方法会报错：
+ *  报错原因：泛型在编译期类型被擦除导致报错
+ *  报错信息：com.google.gson.internal.LinkedTreeMap cannot be cast to Class
+ */
 fun <T> toJsonArray(jsonString: String, cls: Class<T>): List<T> {
     return Gson().fromJson(jsonString, object : TypeToken<List<T>>() {}.type)
+}
+
+fun <T> toJsonList(json: String, clazz: Class<T>): List<T> {
+    if (TextUtils.isEmpty(json)) {
+        throw Exception("jsonString not is null!")
+    } else {
+        var list = mutableListOf<T>()
+        val array = JsonParser().parse(json).asJsonArray
+        array.forEach {
+            list.add(Gson().fromJson(it, clazz))
+        }
+        return list
+    }
+
 }
